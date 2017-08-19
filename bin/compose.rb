9da35c8f3c337e756f5b7a5ee416102add47c8ce
clean_li <clean_li@yahoo.com>
2017-08-19 11:46:21 +0800

    ruby compose cate ok

--- bin/compose.rb
+++ bin/compose.rb
@@ -1,6 +1,24 @@
 #!/usr/bin/ruby -w
 # -*- coding: UTF-8 -*-
 
+def getCat
+	cateStr = String.new("探索 思考 感悟 生活 技术 其它")
+	cateArray = cateStr.split(' ')
+	@i = 0
+	while @i < cateArray.size  do
+		puts("#@i " + cateArray[@i] )
+		@i += 1
+	end
+	puts "Enter cates<5>:"
+	mychs = String.new(gets)
+	mychs.chomp!
+	mychs.strip!
+	if mychs.empty?
+		mychs = mychs + '5'
+	end
+	cateArray[mychs.to_i]
+end
+
 time = Time.new
  
 puts "Enter prefered filename:"
@@ -12,6 +30,7 @@ filename = myStr
 if myStr.empty?
 	puts "null invalid filename, quit"
 else
+	puts "Got filename:"
 	puts myStr
 	up_myStr = myStr.upcase
 	myStr[0] = up_myStr[0] unless up_myStr.empty?
@@ -23,23 +42,37 @@ else
 		puts filename 
 	end
 
+	puts "Enter title:"
+	myTitle = String.new(gets)
+	myTitle.chomp!
+	myTitle.strip!
+
+	puts "Enter subtitle:"
+	mySubtitle = String.new(gets)
+	mySubtitle.chomp!
+	mySubtitle.strip!
+	myCate = getCat
+	puts "Choose:"
+	puts myCate
+
 	puts "---"
 	puts "layout: post"
-	puts "title: TBD"
+	puts "title: " + '"' + myTitle + '"'
 	puts "date: " + time.strftime("%Y-%m-%d %H:%M:%S %z")
-	puts "subtitle:"
-	puts "categories:"
+	puts "subtitle: " + '"' + mySubtitle + '"' unless mySubtitle.empty?
+	puts "categories: " + '"' + myCate + '"'
 	puts "tags:"
 	puts "---"
 
 	aFile = File.new(filename, "a+")
 	aFile.puts "---"
 	aFile.puts "layout: post"
-	aFile.puts "title: TBD"
+	aFile.puts "title: " + '"' + myTitle + '"'
 	aFile.puts "date: " + time.strftime("%Y-%m-%d %H:%M:%S %z")
-	aFile.puts "subtitle:"
-	aFile.puts "categories:"
+	aFile.puts "subtitle: " + '"' + mySubtitle + '"' unless mySubtitle.empty?
+	aFile.puts "categories: " + '"' + myCate + '"'
 	aFile.puts "tags:"
 	aFile.puts "---"
 	aFile.close
 end
+
